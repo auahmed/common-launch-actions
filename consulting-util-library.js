8bd3cac8c6cddfdf626ce5fbff3373799e2333ac
Attiq Ahmed <attiqahmed92@gmail.com>
2021-05-20 09:52:52 -0400

    Create consulting-util-library.js

---
+++ consulting-util-library.js
@@ -0,0 +1,8 @@
+/**
+ * JS Util functions for Adobe Launch Data Elements and Rules.
+ * 
+ * version: 1.0.0
+ */
+window.adobe = window.adobe || {};
+window.adobe.consulting = window.adobe.consulting || {};
+window.adobe.consulting.util = window.adobe.consulting.util || {};
